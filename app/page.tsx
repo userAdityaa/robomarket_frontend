6c6f6387c4bc01fdcf433680e0504197ca7faa91
userAdityaa <aditya.chaudhary1558@gmail.com>
2025-01-06 19:37:56 +0545

    some image modification

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,62 @@
+import { Space_Mono, Archivo } from "next/font/google";
 import Image from "next/image";
+
+const spaceMono = Space_Mono({
+  weight: '400',
+  subsets: ['latin']
+});
+
+const archivo = Archivo({
+  weight: '900',
+  subsets: ['latin']
+})
 
 export default function Home() {
   return (
-    <>
-    </>
+    <main className={`bg-zinc-900 min-h-screen text-white p-8 ${spaceMono.className} py-28`}>
+      <div className="max-w-7xl mx-auto flex items-center justify-between">
+        <div className="max-w-xl">
+          <p className="text-[#6153CC] mb-4 flex items-center gap-2 font-bold"> 
+            <Image src="/images/Sparkling.svg" alt="sparkling" width={20} height={20}></Image>
+            Mint is paused
+          </p>
+          
+          <h1 className={`text-6xl font-bold mb-6 ${archivo.className}`}>
+            Get Your
+            <br />
+            SuperRobot
+          </h1>
+          
+          <p className="text-xl mb-8 font-mono leading-tight">
+            Collect and customize digital characters
+            <br />
+            First mint is free, no gas fees
+          </p>
+          
+          <div className="flex gap-4 mb-12">
+            <button className="bg-gradient-to-r from-orange-400 to-pink-500 px-6 py-3 rounded-lg font-mono font-bold">
+              Mint for free
+            </button>
+            <div className="flex items-center">
+              <Image src = '/images/Item.png' alt="item no gas" width={140} height={100}></Image>
+            </div>
+          </div>
+          
+          <div className="flex items-center gap-2">
+            <Image src = '/images/Minted.svg' alt="minted people" width={280} height={40}></Image>
+          </div>
+        </div>
+
+        <div className="relative">
+          <Image
+            src="/images/Robot.png"
+            alt="SuperRobot Character"
+            width={1800}
+            height={600}
+            className="relative z-10 w-[200rem]"
+          />
+        </div>
+      </div>
+    </main>
   );
 }
