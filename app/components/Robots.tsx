a989affa2660ea3ddcfa97a0bec60d21f79857c6
userAdityaa <aditya.chaudhary1558@gmail.com>
2025-01-07 00:21:29 +0545

    started out with my robots page.

---
+++ app/components/Robots.tsx
@@ -0,0 +1,4 @@
+export default function Robots() { 
+    return <div></div>
+}
+
--- app/components/index.ts
+++ app/components/index.ts
@@ -1,5 +1,7 @@
 import Navbar from "./navbar";
+import Robots from "./Robots";
 
 export { 
     Navbar,
+    Robots,
 }
